20097c47ade9dc8ffaba2376c87c7cd7932c2a1d
Juan Alfonso <JuanAlfonso.Plata@solera.com>
2022-12-16 12:28:37 +0100

    Init project 1

---
+++ src/main/java/org/bootcamp4/api0009/MainClass.java
@@ -0,0 +1,11 @@
+package org.bootcamp4.api0009;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+}
---
+++ src/test/java/org/bootcamp4/api0009/TestClass.java
@@ -0,0 +1,12 @@
+package org.bootcamp4.api0009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestClass {
+    @Test
+    void contextLoads() {
+
+    }
+}
